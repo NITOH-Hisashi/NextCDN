989549a1fc62713187a687a8947ea8915adc46cb
にとう ひさし <97216430+NITOH-Hisashi@users.noreply.github.com>
2025-07-26 19:37:08 +0900

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 function Page() {
   return (
     <Main>
-      <Image src="../public/profile.png" alt="Profile" width={100} height={100} />
+      <img src="../public/profile.png" alt="Profile" width={100} height={100} />
       <h1>Hello, NextCDN!</h1>
     </Main>
   )
