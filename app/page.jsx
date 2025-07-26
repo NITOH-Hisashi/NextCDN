0313e29e37d3c3fab0f7ca8d01619d84c0f81544
にとう ひさし <97216430+NITOH-Hisashi@users.noreply.github.com>
2025-07-26 18:46:52 +0900

    Update and rename page.js to page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+ 
+export default function Page() {
+  return (
+   <Image src="../public/profile.png" alt="Profile" width={100} height={100} />
+   <h1>Hello, NextCDN!</h1>
+}
